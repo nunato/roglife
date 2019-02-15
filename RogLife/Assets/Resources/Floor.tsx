578d9fd8dd7e7b889bc53f33c08334abaf8dc437
nunato <kyrie601@yahoo.co.jp>
2019-02-15 21:04:29 +0900

    マップタイル更新

---
+++ RogLife/Assets/Resources/Floor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Floor" tilewidth="32" tileheight="32" tilecount="3" columns="3">
+ <image source="Floor.png" trans="ff00ff" width="96" height="32"/>
+</tileset>
