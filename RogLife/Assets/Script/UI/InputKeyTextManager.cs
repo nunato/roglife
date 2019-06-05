225f3f2771b79a020efec0c389a54860560cad17
nunato <kyrie601@yahoo.co.jp>
2019-06-05 19:33:01 +0900

    キーの表示

---
+++ RogLife/Assets/Script/UI/InputKeyTextManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputKeyTextManager : MonoBehaviour
+{
+	[SerializeField]
+	public Text _InputKeyText;
+
+	void Start()
+	{
+		_InputKeyText.text = "";
+
+		_InputKeyText.text = _InputKeyText.text + "M: Map\n";
+		_InputKeyText.text = _InputKeyText.text + "S: Save\n";
+		_InputKeyText.text = _InputKeyText.text + "L: Load\n";
+	}
+}
