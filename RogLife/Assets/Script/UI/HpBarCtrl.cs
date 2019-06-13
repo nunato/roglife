663dcc871f3b54dfb190e19827692621173323a9
nunato <kyrie601@yahoo.co.jp>
2019-06-13 20:58:28 +0900

    HPバーの追加

---
+++ RogLife/Assets/Script/UI/HpBarCtrl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpBarCtrl : MonoBehaviour
+{
+    [SerializeField]
+    private Slider _slider;
+    [SerializeField]
+    private Text _hpText;
+
+    private int _hp = 0;
+
+    void Update()
+    {
+        _hp += 1;
+        if( _hp > 100 ){
+            _hp = 0;
+        }
+
+        _slider.value = _hp;
+        _hpText.text = _hp + "/" + _slider.maxValue;
+    }
+}
